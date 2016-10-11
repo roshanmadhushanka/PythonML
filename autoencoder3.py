893f94970a55092ce035f4e09a54db98b078ef92
Roshan Madhushanka <roshanalwis22@gmail.com>
2016-10-11 11:37:50 +0530

    Auto encoders 3 Feature Engineering

--- autoencoder3.py
+++ autoencoder3.py
@@ -3,6 +3,8 @@ import pandas as pd
 import numpy as np
 from h2o.estimators import H2OAutoEncoderEstimator
 from h2o.estimators import H2ODeepLearningEstimator
+from tqdm import tqdm, tnrange
+from tqdm import trange
 
 from dataprocessor import ProcessData
 from featureeng import Progress
@@ -17,22 +19,19 @@ response_column = 'RUL'
 h2o.init()
 
 # Load data frames
-pData = ProcessData.trainData(moving_k_closest_average=True, standard_deviation=True, probability_distribution=True)
-pTest = ProcessData.testData(moving_k_closest_average=True, standard_deviation=True, probability_from_file=True)
+pData = ProcessData.trainData()
+#pTest = ProcessData.testData()
 
 # Split data frame
 pValidate = pData.sample(frac=_validation_ratio, random_state=200)
 pTrain = pData.drop(pValidate.index)
 
-# Convert pandas to h2o frame
+# Convert pandas to h2o frame - for anomaly detection
 hValidate = h2o.H2OFrame(pValidate)
 hValidate.set_names(list(pValidate.columns))
 
 hTrain = h2o.H2OFrame(pTrain)
 hTrain.set_names(list(pTrain.columns))
-
-hTest = h2o.H2OFrame(pTest)
-hTest.set_names(list(pTest.columns))
 
 # Save validate and train frames
 pValidate.to_csv("Auto-Validate.csv", index=False)
@@ -74,17 +73,31 @@ print "-------------------------------------------------------------------------
 print "Reconstruction Error Array Size :", len(reconstruction_error)
 filtered_train = pd.DataFrame()
 count = 0
-for i in range(hTrain.nrow):
+for i in trange(hTrain.nrow):
     if reconstruction_error[i,0] < threshold:
         df1 = pTrain.iloc[i, :]
         filtered_train = filtered_train.append(df1, ignore_index=True)
         count += 1
-    Progress.printProgress(iteration=(i+1), total=hTrain.nrow, decimals=1, prefix="Progress", suffix="Complete")
+    #Progress.printProgress(iteration=(i+1), total=hTrain.nrow, decimals=1, prefix="Progress", suffix="Complete")
 
 print filtered_train
 print "Original Size :", hTrain.nrow
 print "Filtered Size :", len(filtered_train)
 print "Removed Rows  :", (hTrain.nrow-len(filtered_train))
+
+# Feature Engineering
+pData = ProcessData.trainDataToFrame(filtered_train, moving_k_closest_average=True, standard_deviation=True, probability_distribution=True)
+pTest = ProcessData.testData(moving_k_closest_average=True, standard_deviation=True, probability_from_file=True)
+
+# Convert pandas to h2o frame - for model training
+hValidate = h2o.H2OFrame(pValidate)
+hValidate.set_names(list(pValidate.columns))
+
+hTrain = h2o.H2OFrame(pTrain)
+hTrain.set_names(list(pTrain.columns))
+
+hTest = h2o.H2OFrame(pTest)
+hTest.set_names(list(pTest.columns))
 
 # Training model
 print "\nTraining Model"
